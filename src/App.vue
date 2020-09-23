39f6259843aad2c9e7f14c975fd98d0f777ec21a
xushilong0812 <985540299@qq.com>
2020-09-23 15:16:34 +0800

    add

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+         <!-- 挂载到 DOM -->
+      <FootberNav/>
+
+
+
+
+
+  </div>
+</template>
+<script>
+import FootberNav from "@/components/FootberNav/FootberNav"
+
+export default {
+components:{
+  FootberNav,
+}
+  
+}
+</script>
+<style lang="stylus">
+
+</style>
---
+++ src/components/HeaderTop/HeaderTop.vue
@@ -0,0 +1,23 @@
+<template>
+   <header class="header">
+            <a class="header_title">
+              <span class="header_title_text">{{title}}</span>
+            </a>
+            <slot name="right"></slot>
+          </header>
+</template>
+
+<script>
+export default {
+            props:{title:String},
+     
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang='stylus' type='text/stylus' >
+
+  
+</style>
